e1428da29f59eb19a7c9d3b851149a97d84ef487
Ashok Raj <ashokraj.info@gmail.com>
2025-09-07 15:23:48 -0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'watchnext-v1';
+const ASSETS = ['/', '/index.html', '/styles.css', '/main.js', '/manifest.webmanifest'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k!==CACHE).map(k => caches.delete(k)))));
+});
+self.addEventListener('fetch', (e) => {
+  const { request } = e;
+  if (request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(request).then(cached => cached || fetch(request))
+  );
+});

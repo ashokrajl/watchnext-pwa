8921448d6191dc24e0370935344c5045cb309707
Ashok Raj <ashokraj.info@gmail.com>
2025-09-07 19:19:13 -0700

    Update tmdb.js to use mode query parameter and fix header

--- api/tmdb.js
+++ api/tmdb.js
@@ -1,24 +1,27 @@
 export default async function handler(req, res) {
-  const { pathname, searchParams } = new URL(req.url, `http://${req.headers.host}`);
-  // Determine endpoint based on path
+const { searchParams } = new URL(req.url, 'http://localhost');
+  const mode = searchParams.get('mode') || 'discover';
   const page = searchParams.get('page') || '1';
   const genres = searchParams.get('genres');
   const from = searchParams.get('from');
   const to = searchParams.get('to');
+
   let endpoint;
-  if (pathname.endsWith('/genres')) {
+  if (mode === 'genres') {
     endpoint = '/genre/movie/list';
   } else {
     endpoint = '/discover/movie';
   }
+
   const qs = new URLSearchParams({ page, language: 'en-US', sort_by: 'popularity.desc' });
   if (genres) qs.set('with_genres', genres);
   if (from) qs.set('primary_release_date.gte', from);
   if (to) qs.set('primary_release_date.lte', to);
-  const url = `https://api.themoviedb.org/3${endpoint}?${qs.toString()}`;
+
+  const url = 'https://api.themoviedb.org/3' + endpoint + '?' + qs.toString();
   const response = await fetch(url, {
     headers: {
-      Authorization: `Bearer ${process.env.TMDB_BEARER || ''}`,
+     AAuthorization: 'Bearer ' + process.env.TMDB_BEARER
     },
   });
   const data = await response.json();

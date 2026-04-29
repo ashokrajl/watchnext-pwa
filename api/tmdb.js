9fb1231a009ed539147dc91b47b4e5160398dc89
Ashok Raj <ashokraj.info@gmail.com>
2026-04-29 11:21:43 -0400

    Use CommonJS for Vercel API function

--- api/tmdb.js
+++ api/tmdb.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   const { pathname, searchParams } = new URL(req.url, `http://${req.headers.host || 'localhost'}`);
   const mode = searchParams.get('mode') || 'discover';
   const isGenresRequest = mode === 'genres' || pathname.endsWith('/genres');
